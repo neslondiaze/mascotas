c69eed5be1ae9160bf542ce58ee4eb8b9a47433e
neslondiaze <neslondiaze>
2021-09-28 16:14:59 -0400

    Actualizacion de app liea 43

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const http = require('http');
 const app = express();
+
+
+
 const server = http.createServer(app);
 
 require('dotenv').config()
--- router/Mascotas.js
+++ router/Mascotas.js
@@ -5,20 +5,14 @@ const Mascota = require('../models/mascota')
 
 router.get('/', async (req, res) => {
     
-    try {
-        
+    try {        
         const arrayMascotasDB = await Mascota.find()
-        console.log(arrayMascotasDB)
-
         res.render("mascotas", {
             arrayMascotas: arrayMascotasDB
         })
-
     } catch (error) {
         console.log(error)
-    }
-
-    
+    }  
 })
 
 module.exports = router;
